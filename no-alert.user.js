1495c5d61c3f0b71ba5179e1f5e613221d1ac304
binsee <5285894+binsee@users.noreply.github.com>
2021-10-22 23:20:27 +0800

    update no-alert

--- no-alert.user.js
+++ no-alert.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         No alert
-// @version      0.1.0
+// @version      0.1.1
 // @description  Don't pop up alert, use console.log
+// @description:zh-cn  屏蔽网站使用alert弹出提示框，改用console.log，以提高页面使用体验。
 // @author       binsee
 // @namespace    https://github.com/binsee/tampermonkey-scripts
 // @supportURL   https://github.com/binsee/tampermonkey-scripts/issues
