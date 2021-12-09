e684353c159c632ba88c6a8415c3b54895a3a03b
binsee <5285894+binsee@users.noreply.github.com>
2021-12-09 19:53:51 +0800

    update bili-live-jump-simple-room.user.js

--- bili-live-jump-simple-room.user.js
+++ bili-live-jump-simple-room.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         不显示B站直播间活动皮肤
-// @version      0.0.1
+// @version      0.0.2
 // @description  Jump to the simple version of the live broadcast room.
 // @description:zh-cn  让有活动皮肤的B站直播间，跳转到直播间原始样式，还原简洁的观看体验。
 // @author       binsee
@@ -16,7 +16,8 @@
 (function () {
   'use strict';
   if (window.__initialState?.BaseInfo?.__activity_id) {
-    const roomId = window.__initialState['live-non-revenue-player'][0].defaultRoomId
+    const tmp = window.location.pathname.match(/^\/(\d+)/)
+    const roomId = tmp ? tmp[1] : window.__initialState['live-non-revenue-player'][0].defaultRoomId
     const url = 'https://live.bilibili.com/blanc/' + roomId
     console.log('【不要显示B站直播活动皮肤】', '即将跳转到', url)
     window.location.href = url
