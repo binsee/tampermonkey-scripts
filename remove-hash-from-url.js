9dffbf1e3659701606ab67bf0180671b14dd1c71
binsee <5285894+binsee@users.noreply.github.com>
2021-12-15 22:34:10 +0800

    update remove-hash-from-url.js

--- remove-hash-from-url.js
+++ remove-hash-from-url.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         移除网址中的锚点
-// @version      0.0.1
-// @description  Remove hash from URL, such as Douban.
-// @description:zh-cn  移除网址中的锚点，例如豆瓣。
+// @version      0.0.2
+// @description  Remove hash from URL, such as Douban group.
+// @description:zh-cn  移除网址中的锚点，例如豆瓣小组。
 // @author       binsee
 // @namespace    https://github.com/binsee/tampermonkey-scripts
 // @supportURL   https://github.com/binsee/tampermonkey-scripts/issues
 // @license      GPL
 // @noframes
-// @match        https://www.douban.com/group/topic/*
+// @match        *://www.douban.com/group/topic/*
 // @grant        none
 // ==/UserScript==
 
@@ -20,5 +20,5 @@
     } else {
       window.location.replace('#');
     }
-  }, 300);
+  }, 500);
 })();
